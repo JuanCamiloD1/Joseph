4e8e5e923c361fa6c95c72bc49813e8dbb56e11f
JuanCamiloD1 <juancamilodiazbasto@gmail.com>
2020-08-08 13:40:26 -0500

    bot deploy

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {
+    token: "NzQxNzExNDc5ODA3NjcyMzgz.Xy7ipQ.FJbIk86ZAgygIgHMDBEsJO4Lyqk", // Reemplaze esto con el token de tu de bot.
+    prefix: "/", // El prefix de su bot para indentificar sus comandos.
+  
+  }
+  
+  // Aqui exportamos el objeto config:
+  module.exports = config;
+  
+  
